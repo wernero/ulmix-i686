b0fc7ba3941831b7bf4907907b6b535fec70c397
Rainer Rudigier <rainer@rudigier.at>
2018-12-05 21:30:20 +0100

    offset of losetup angepasst auf 512

--- diskimage/create_image.sh
+++ diskimage/create_image.sh
@@ -8,7 +8,7 @@ bximage -mode=create -hd=30M -sectsize=512 -q hdd.img
 sfdisk hdd.img < sfdisk.layout
 sfdisk -l hdd.img
 
-losetup -o 32256 --sizelimit 30965248 /dev/loop1 hdd.img
+losetup -o 512 --sizelimit 30965248 /dev/loop1 hdd.img
 mkfs.ext2 /dev/loop1
 
 mkdir /mnt/hd1p1 ; mount /dev/loop1 /mnt/hd1p1
