51b5aa7f2a08628403436de87c64dd638738a7d4
Alexander Ulmer <alexulmer1999@gmail.com>
2018-12-16 22:06:00 +0100

    preparing syscalls for libc

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -5,29 +5,32 @@
 
 #define nop 0
 
+/* every system call that is marked with *required is actually required
+ * by the Newlib C Library and thus has to be implemented */
+
 void *syscalls[] =
 {
     nop,                // 0
-    sc_exit,            // 1    exit()
-    sc_fork,            // 2    fork()
-    sc_read,            // 3    read()
-    sc_write,           // 4    write()
-    sc_open,            // 5    open()
-    sc_close,           // 6    close()
-    sc_waitpid,         // 7    waitpid()
+    sc_exit,            // 1    exit()              *required
+    sc_fork,            // 2    fork()              *required
+    sc_read,            // 3    read()              *required
+    sc_write,           // 4    write()             *required
+    sc_open,            // 5    open()              *required
+    sc_close,           // 6    close()             *required
+    sc_waitpid,         // 7    waitpid()           *required
     sc_creat,           // 8    creat()
-    sc_link,            // 9    link()
-    sc_unlink,          // 10   unlink()
-    sc_execve,          // 11   execve()
+    sc_link,            // 9    link()              *required
+    sc_unlink,          // 10   unlink()            *required
+    sc_execve,          // 11   execve()            *required
     nop,                // 12   chdir()
     nop,                // 13   time()
     nop,                // 14   mknod()
     nop,                // 15   chmod()
     nop,                // 16   lchown16()
     nop,                // 17
-    nop,                // 18   stat()
-    sc_lseek,           // 19   lseek()
-    nop,                // 20   getpid()
+    nop,                // 18   stat()              *required
+    sc_lseek,           // 19   lseek()             *required
+    nop,                // 20   getpid()            *required
     nop,                // 21   mount()
     nop,                // 22   oldumount()
     nop,                // 23   setuid16()
@@ -35,7 +38,7 @@ void *syscalls[] =
     nop,                // 25   stime()
     nop,                // 26   ptrace()
     nop,                // 27   alarm()
-    nop,                // 28   fstat()
+    nop,                // 28   fstat()             * required
     nop,                // 29   pause()
     nop,                // 30   utime()
     nop,                // 31
@@ -44,18 +47,20 @@ void *syscalls[] =
     nop,                // 34   nice()
     nop,                // 35
     nop,                // 36   sync()
-    nop,                // 37   kill()
+    nop,                // 37   kill()              * required
     nop,                // 38   rename()
     nop,                // 39   mkdir()
     nop,                // 40   rmdir()
     nop,                // 41   dup()
     nop,                // 42   pipe()
-    nop,                // 43   times()
+    nop,                // 43   times()             *required
     nop,                // 44
-    nop,                // 45   brk()
+    nop,                // 45   brk()               *required
     nop,                // 46   setgid16()
     nop,                // 47   getgid16()
     nop,                // 48   signal()
     nop,                // 49   geteuid16()
     nop,                // 50
+
+    nop,                // 78	gettimeofday()      *required
 };
