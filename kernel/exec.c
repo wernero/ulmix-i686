6e4f675be14da5b180a235af874aee3749e15857
Rainer Rudigier <rainer@rudigier.at>
2018-12-24 17:48:59 +0100

    small fix ext2_read

--- kernel/exec.c
+++ kernel/exec.c
@@ -75,7 +75,7 @@ static int exec_load_img(pagedir_t *pagedir, char *filename, void **entry)
     char *file_load_buffer = kmalloc(current_thread->process->files[fd]->direntry->size, 1, "file exec buffer");
 
     sc_lseek(fd, 0x20, SEEK_SET);
-    sc_read(fd,file_load_buffer, current_thread->process->files[fd]->direntry->size);
+    sc_read(fd,file_load_buffer, 0x100);
 
     hexdump(KLOG_INFO, file_load_buffer, 0x100);
 
