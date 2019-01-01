3a89f50eb2aefc86906668149dcf94f70fe7123d
Alexander Ulmer <alexulmer1999@gmail.com>
2019-01-01 16:55:03 +0100

    shell test program

--- userspace/ushell/ushell.c
+++ userspace/ushell/ushell.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 
+static void readline(char *buf)
+{
+    char c;
+    while ((c = getchar()) != '\n')
+        *(buf++) = c;
+    *buf = 0;
+}
+
 int main(void)
 {
     puts("ULMIX shell\n");
 
+    char buf[256];
+    readline(buf);
+
+    puts("have read the following line: \n");
+    puts(buf);
 
 
     return 0;
 }
+
