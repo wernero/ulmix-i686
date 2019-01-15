b79c2c7bfdbd04258324af308115f664f9fbfd3d
Alexander Ulmer <alexulmer1999@gmail.com>
2019-01-15 21:38:28 +0100

    minor form correction

--- kernel/filesystem/ext2.c
+++ kernel/filesystem/ext2.c
@@ -75,19 +75,15 @@ static int ext2_mount(struct filesystem_struct *fs, struct dir_struct *mountpoin
     // initalize sb_struct in dir_struct
     mountpoint->sb = kmalloc(sizeof(struct sb_struct), 1 , "sb_struct");
 
-    mountpoint->sb->fs = fs; // to fix function calls to ext2 FS functions which
-                             // are installed via install_fs .. (filesystem.c)
+    mountpoint->sb->fs = fs;
     mountpoint->sb->s_blocks_total = superblock->total_blocks;
     mountpoint->sb->s_inodes_total = superblock->total_inodes;
     mountpoint->sb->s_blocks_per_group = superblock->blocks_per_group;
     mountpoint->sb->s_inodes_per_group = superblock->inodes_per_group;
 
-    // how many blocks do we have
     mountpoint->sb->s_gdb_count = superblock->total_blocks / superblock->blocks_per_group + 1; // **TODO** correct round up
 
     kfree(superblock);
-
-
 
     //  struct hd_struct *partition = &(bd->part_list[part]);
     char *group_descriptor_buf;
