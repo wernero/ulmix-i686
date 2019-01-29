4f4c37573ae20095275ec0b00fa295ecec1fe4bd
Alexander Ulmer <alexulmer1999@gmail.com>
2019-01-29 18:52:45 +0100

    add grub bootloader to diskimage

--- diskimage/create_image.sh
+++ diskimage/create_image.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 
-mv hdd.img hdd.img.bak
+rm -f hdd.img
+mkdir -p mnt
 
-rm -f hdd.img 
-sudo mkdir -p mnt
+cp hdd.img.template hdd.img
 
-dd if=/dev/zero of=hdd.img count=40960 # 20MB
-sfdisk hdd.img < sfdisk.layout
-sfdisk -l hdd.img
+sudo umount /mnt 2> /dev/null
+sudo losetup -d /dev/loop20 2> /dev/null
+sudo losetup /dev/loop20 hdd.img -P
 
-sudo umount /dev/loop1
-sudo losetup -o 512 --sizelimit 30965248 /dev/loop1 hdd.img
-sudo mkfs.ext2 /dev/loop1
+sudo mount /dev/loop20p1 /mnt
 
-sudo mount /dev/loop1 mnt
+sudo mkdir /mnt/bin
+sudo mkdir /mnt/dev
+sudo mknod /mnt/dev/tty c 1 0
+sudo mknod /mnt/dev/tty1 c 1 1
+sudo mknod /mnt/dev/tty2 c 1 2
+sudo mknod /mnt/dev/kbd c 2 0
+sudo mknod /mnt/dev/hda c 8 0
+sudo mknod /mnt/dev/hda1 c 8 1
 
-sudo mkdir mnt/bin
-sudo mkdir mnt/dev
-sudo mknod mnt/dev/tty c 1 0
-sudo mknod mnt/dev/tty1 c 1 1
-sudo mknod mnt/dev/tty2 c 1 2
-sudo mknod mnt/dev/kbd c 2 0
-sudo mknod mnt/dev/hda c 8 0
-sudo mknod mnt/dev/hda1 c 8 1
-sudo cp ../userspace/init/init.elf mnt/bin/init
-sudo cp ../userspace/hexdump/hexdump.elf mnt/bin/hexdump
-sudo cp ../userspace/ushell/ushell.elf mnt/bin/ush
+sudo cp grub.cfg /mnt/boot/grub/grub.cfg
+sudo cp ../vmulmix.elf /mnt/vmulmix.elf
 
-sudo umount mnt
-sudo losetup -D
+sudo cp ../userspace/init/init.elf /mnt/bin/init
+sudo cp ../userspace/hexdump/hexdump.elf /mnt/bin/hexdump
+sudo cp ../userspace/ushell/ushell.elf /mnt/bin/ush
+
+sudo umount /mnt
+sudo losetup -d /dev/loop20
 #sudo rm -rf mnt
--- kernel/kernel.s
+++ kernel/kernel.s
@@ -11,15 +11,24 @@
 
 jmp kernel_start
 
-MB_FLAGS	equ 0x02
 MB_MAGIC	equ 0x1badb002
+MB_FLAGS	equ 0x2 ; 0x10002 for flat binary
 MB_CHECKSUM	equ -(MB_MAGIC + MB_FLAGS)
+MB_LOAD_ADDR    equ 0x100000
+MB_LOAD_END     equ 0x00        ; load the whole image
+MB_BSS_END      equ 0x00        ; we clear BSS ourselves
+MB_ENTRY_ADDR   equ 0x100000
 
 align 4
 multiboot_header:
     dd MB_MAGIC
     dd MB_FLAGS
     dd MB_CHECKSUM
+    dd (multiboot_header)
+    dd MB_LOAD_ADDR
+    dd MB_LOAD_END
+    dd MB_BSS_END
+    dd MB_ENTRY_ADDR
 
 kernel_start:
     mov esp, 0x600000
