0e59475e440fc77ba6dabbe7a33d9993f7f3e78c
Alexander Ulmer <alexulmer1999@gmail.com>
2019-02-02 17:09:46 +0100

    add network device to launch script

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-qemu-system-i386 -kernel vmulmix.elf -drive format=raw,file=diskimage/hdd.img -serial file:serial &
+qemu-system-i386 -kernel vmulmix.elf -drive format=raw,file=diskimage/hdd.img -serial file:serial -device rtl8139,netdev=net0 -netdev user,id=net0 &
 cat serial
