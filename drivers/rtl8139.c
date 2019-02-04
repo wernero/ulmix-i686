6a31f06ca15df62a68d5e62980939f6484281da0
Alexander Ulmer <alexulmer1999@gmail.com>
2019-02-04 19:40:11 +0100

    RTL8139 can receive packets

--- drivers/rtl8139.c
+++ drivers/rtl8139.c
@@ -7,18 +7,33 @@
 #include <kdebug.h>
 #include <devices/pci.h>
 #include <memory/kheap.h>
+#include <interrupts.h>
 
 #define DRVNAME "rtl8139"
 
 enum rtl8139_registers
 {
+    tx_cmd0         = 0x10,
+    tx_cmd1         = 0x14,
+    tx_cmd2         = 0x18,
+    tx_cmd3         = 0x1c,
+    tx_buf0         = 0x20,
+    tx_buf1         = 0x24,
+    tx_buf2         = 0x28,
+    tx_buf3         = 0x2c,
+    rx_buf          = 0x30,
+    intr_mask       = 0x3c,
+    intr_status     = 0x3e,
     chip_cmd        = 0x37,
-    config1         = 0x52
+    config1         = 0x52,
+    rx_config       = 0x44
 };
 
 struct rtl8139dev
 {
     uint32_t iobase;
+    int interrupt;
+    unsigned char mac[6];
 };
 
 static pci_device_id_t idtable[] =
@@ -46,12 +61,40 @@ static pci_device_id_t idtable[] =
     { 0, 0 }
 };
 
+struct eth_pkg
+{
+    unsigned char dest_mac[6];
+    unsigned char src_mac[6];
+    uint16_t type;
+};
+
+struct rtl8139dev *info;
+unsigned char rx_buffer[8192 + 16]; // receive buffer
+
+static void rtl8139_intr(void)
+{
+    if (info == NULL)
+        return;
+
+    /*struct eth_pkg *pkg = (struct eth_pkg*)rx_buffer;
+    klog(KLOG_INFO, "dest=%x, src=%x, type=%x",
+         *((unsigned long*)pkg->dest_mac), *((unsigned long*)pkg->src_mac), pkg->type);
+    */
+
+    klog(KLOG_INFO, DRVNAME ": received packet");
+    outw(info->iobase + intr_status, 0x01);
+}
+
 static int rtl8139_probe(pci_device_t *dev)
 {
     klog(KLOG_INFO, "RTL8139 Fast Ethernet Controller");
     dev->drv_struct = kmalloc(sizeof(struct rtl8139dev), 1, "rtl8139 drv");
-    struct rtl8139dev *info = (struct rtl8139dev*)dev->drv_struct;
-    info->iobase = pci_read32(dev, PCI_BAR0);
+    info = (struct rtl8139dev*)dev->drv_struct;
+    info->iobase = pci_read32(dev, PCI_BAR0) & 0xfffffffe;
+    info->interrupt = pci_read8(dev, PCI_INTR_LINE) + 32;
+
+    klog(KLOG_DEBUG, DRVNAME ": intr-pin=%d, intr-line=%d",
+         pci_read8(dev, PCI_INTR_PIN), pci_read8(dev, PCI_INTR_LINE));
 
     klog(KLOG_DEBUG, DRVNAME ": DMA: enable PCI bus mastering");
     uint16_t pci_command = pci_read16(dev, PCI_COMMAND);
@@ -62,7 +105,21 @@ static int rtl8139_probe(pci_device_t *dev)
     outb(info->iobase + config1, 0x00);
     outb(info->iobase + chip_cmd, 0x10);
     while (inb(info->iobase + chip_cmd) & 0x10);
-    klog(KLOG_DEBUG, DRVNAME ": reset successful");
+
+    klog(KLOG_DEBUG, DRVNAME ": init RX buffer (0x%x)", (unsigned long)rx_buffer);
+    outl(info->iobase + rx_buf, (uint32_t)rx_buffer);
+
+    klog(KLOG_DEBUG, DRVNAME ": enable interrupt for TX_OK, RX_OK");
+    irq_install_handler(info->interrupt, rtl8139_intr);
+    outw(info->iobase + intr_mask, 0x0005);
+
+    klog(KLOG_DEBUG, DRVNAME ": RX config set to promiscous");
+    outl(info->iobase + rx_config, 0xf | (1 << 7));
+
+    klog(KLOG_DEBUG, DRVNAME ": Enable RX and TX");
+    outb(info->iobase + chip_cmd, 0x0c);
+
+    //rtl8139_write(info);
 
     return 0;
 }
--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-qemu-system-i386 -kernel vmulmix.elf -drive format=raw,file=diskimage/hdd.img -serial file:serial -device rtl8139,netdev=net0 -netdev user,id=net0 &
+qemu-system-i386 -kernel vmulmix.elf -drive format=raw,file=diskimage/hdd.img -serial file:serial -netdev tap,id=br0 -device rtl8139,netdev=br0,mac=ab:cd:ef:ab:cd:ef &
 cat serial
