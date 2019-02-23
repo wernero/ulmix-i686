f13475a43ad11ed3f27532553f6e64e3d1db6448
Alexander Ulmer <alexulmer1999@gmail.com>
2019-02-23 11:21:16 +0100

    actually set errno in userspace functions

--- userspace/stdlib/dirent.c
+++ userspace/stdlib/dirent.c
@@ -2,17 +2,22 @@
 #include <errno.h>
 #include <stdlib.h>
 
+extern int actual_errno;
+
 DIR *opendir(char *name)
 {
+    actual_errno = ENOSYS;
     return NULL;
 }
 
 int closedir(DIR *dirp)
 {
-    return -ENOSYS;
+    actual_errno = ENOSYS;
+    return -1;
 }
 
 struct dirent* readdir(DIR *dirp)
 {
+    actual_errno = ENOSYS;
     return NULL;
 }
