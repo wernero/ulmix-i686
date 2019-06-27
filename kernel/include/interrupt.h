1aa483de2b4778043b9bd7a75886da13bc5712e1
Alexander Ulmer <alexulmer1999@gmail.com>
2019-06-27 18:54:57 +0200

    register IRQs

--- kernel/include/interrupt.h
+++ kernel/include/interrupt.h
@@ -1,7 +1,7 @@
 #ifndef IRQ_H
 #define IRQ_H
 
-void register_irq(unsigned irq, void (*handler)(void));
-void unregister_irq(unsigned irq);
+int register_irq(unsigned irq, void (*handler)(void));
+int unregister_irq(unsigned irq);
 
 #endif // IRQ_H
---
+++ kernel/interrupt.c
@@ -0,0 +1,28 @@
+#include <interrupt.h>
+#include <types.h>
+
+typedef void (*func_t)();
+
+static func_t irq_handlers[256];
+
+int register_irq(unsigned irq, void (*handler)())
+{
+   if (irq_handlers[irq] == NULL)
+   {
+       irq_handlers[irq] = handler;
+       return 0;
+   }
+
+   return -1;
+}
+
+int unregister_irq(unsigned irq)
+{
+    if (irq_handlers[irq] != NULL)
+    {
+        irq_handlers[irq] = NULL;
+        return 0;
+    }
+
+    return -1;
+}
