2ce29739f409aaa8dd99fe4be4c0ae8feee73c25
Alexander Ulmer <alexulmer1999@gmail.com>
2019-07-02 20:24:02 +0200

    add hard disk to launch script

--- debug.sh
+++ debug.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-qemu-system-i386 -kernel vmulmix -serial stdio -S -s
+qemu-system-i386 -kernel vmulmix -serial stdio -drive format=raw,file=diskimage/hdd.img -S -s
--- launch.sh
+++ launch.sh
@@ -2,4 +2,5 @@
 
 qemu-system-i386 \
     -kernel vmulmix \
-    -serial file:/dev/stdout
+    -serial file:/dev/stdout \
+    -drive format=raw,file=diskimage/hdd.img
