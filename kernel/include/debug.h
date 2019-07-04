deaff4830ddafc1ba705469f7e78182d1a87cc67
Alexander Ulmer <alexulmer1999@gmail.com>
2019-07-04 21:41:58 +0200

    introduce debug assertions

--- kernel/include/debug.h
+++ kernel/include/debug.h
@@ -4,4 +4,6 @@
 void panic(const char *fmt, ...);
 void kprintf(const char *fmt, ...);
 
+#define ASSERT(x) if (!(x)) { panic("assertion failed: %s: line %d\n", __FILE__, __LINE__); }
+
 #endif // DEBUG_H
